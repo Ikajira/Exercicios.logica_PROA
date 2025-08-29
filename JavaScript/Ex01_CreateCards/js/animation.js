762b85abea852240002e0eafb82a8f68f500e0aa
Kaique Souza <kaique.souza3101@gmail.com>
2025-08-29 13:49:27 -0300

    add: Ex01

---
+++ JavaScript/Ex01_CreateCards/js/animation.js
@@ -0,0 +1,23 @@
+function exibe_santana_2000(){
+    document.getElementById("santana_2000").style.visibility="visible";
+    document.getElementById("galant").style.visibility="hidden";
+    document.getElementById("gtr_r35").style.visibility="hidden";
+}
+
+function exibe_galant(){
+    document.getElementById("santana_2000").style.visibility="hidden";
+    document.getElementById("galant").style.visibility="visible";
+    document.getElementById("gtr_r35").style.visibility="hidden";
+}
+
+function exibe_gtr_r35(){
+    document.getElementById("santana_2000").style.visibility="hidden";
+    document.getElementById("galant").style.visibility="hidden";
+    document.getElementById("gtr_r35").style.visibility="visible";
+}
+
+function hidden_divs(){
+    document.getElementById("santana_2000").style.visibility="hidden";
+    document.getElementById("galant").style.visibility="hidden";
+    document.getElementById("gtr_r35").style.visibility="hidden";
+}
